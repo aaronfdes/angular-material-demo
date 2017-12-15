f04e894685e285674fe72df19efd29f956c32d36
aaron fernandes <aaronfernandes1406@gmail.com>
2017-12-16 01:56:01 +0530

    Added angular 2 lightbox support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,38 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { Lightbox } from 'angular2-lightbox';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   animal: string;
   name: string;
+  albums = [];
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog,private _lightbox: Lightbox) {}
+
+  ngOnInit(){
+    for (let i = 1; i <= 4; i++) {
+      const src = 'http://www.w3schools.com/w3css/img_fjords.jpg';
+      const caption = 'Image ' + i + ' caption here';
+      const thumb = 'http://www.w3schools.com/w3css/img_fjords.jpg';
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+
+      this.albums.push(album);
+    }
+  }
+
+  open(index: number): void {
+      this._lightbox.open(this.albums, index);
+    }
 
   openDialog(): void {
     let dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import { LightboxModule } from 'angular2-lightbox';
+
 import { MyOwnCustomMaterialModule } from './myownmaterial.module';
 import { DialogOverviewExampleDialog } from './app.component';
 import { AppComponent } from './app.component';
@@ -15,8 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MyOwnCustomMaterialModule
-  ],  
+    MyOwnCustomMaterialModule,
+    LightboxModule
+  ],
   entryComponents: [
     DialogOverviewExampleDialog
     ],
